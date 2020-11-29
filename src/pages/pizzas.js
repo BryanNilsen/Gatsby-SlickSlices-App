141a7a870ac238f4292ab028767437ea85981289
BryanNilsen <bryannilsen@gmail.com>
2020-11-29 16:30:47 -0600

    pizza list created

--- src/pages/pizzas.js
+++ src/pages/pizzas.js
@@ -1,9 +1,39 @@
 import React from 'react';
+import { graphql } from 'gatsby';
+import PizzaList from '../components/PizzaList';
 
-export default function PizzasPage() {
+export default function PizzasPage({ data }) {
+  console.log(data);
+  const pizzas = data.pizzas.nodes;
   return (
     <>
-      <p>Hey! I'm the Pizzas page</p>
+      <PizzaList pizzas={pizzas} />
     </>
   );
 }
+
+export const query = graphql`
+  query PizzaQuery {
+    pizzas: allSanityPizza {
+      nodes {
+        id
+        name
+        slug {
+          current
+        }
+        price
+        toppings {
+          id
+          name
+        }
+        image {
+          asset {
+            fluid(maxWidth: 400) {
+              ...GatsbySanityImageFluid
+            }
+          }
+        }
+      }
+    }
+  }
+`;
