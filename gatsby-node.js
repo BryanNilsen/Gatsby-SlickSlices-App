324ade2322cc7a11ef4b6b5fd635207b77934706
BryanNilsen <bryannilsen@gmail.com>
2020-12-01 23:11:42 -0600

    completed vids 24-25

--- gatsby-node.js
+++ gatsby-node.js
@@ -16,7 +16,6 @@ async function turnPizzasIntoPages({ graphql, actions }) {
       }
     }
   `);
-  console.log(data);
   // 3 loop over each pizza and create a page for that pizza
   data.pizzas.nodes.forEach((pizza) => {
     actions.createPage({
@@ -30,10 +29,45 @@ async function turnPizzasIntoPages({ graphql, actions }) {
   });
 }
 
+async function turnToppingsIntoPages({ graphql, actions }) {
+  console.log('Turning toppings into pages');
+  // 1. get the template
+  const toppingTemplate = path.resolve('./src/pages/pizzas.js');
+  // 2. query all the toppings
+  const { data } = await graphql(`
+    query {
+      toppings: allSanityTopping {
+        nodes {
+          name
+          id
+        }
+      }
+    }
+  `);
+  // 3. create page for that topping
+  data.toppings.nodes.forEach((topping) => {
+    console.log('Creating page for topping', topping.name);
+    actions.createPage({
+      path: `topping/${topping.name}`,
+      component: toppingTemplate,
+      context: {
+        topping: topping.name,
+        // TODO regex for topping
+        toppingRegex: `/${topping.name}/i`,
+      },
+    });
+  });
+  // 4. Pass topping data to pizza.js
+}
+
 export async function createPages(params) {
   //  create pages dynamically
+  // wait for all promises to be resolved before finishing this function
+  await Promise.all([
+    turnPizzasIntoPages(params),
+    turnToppingsIntoPages(params),
+  ]);
   // 1 pizzas
-  await turnPizzasIntoPages(params);
   // 2 toppings
   // slicemasters
 }
--- src/templates/Pizza.js
+++ src/templates/Pizza.js
@@ -1,16 +1,27 @@
 import React from 'react';
 import { graphql } from 'gatsby';
 import Img from 'gatsby-image';
+import styled from 'styled-components';
 
-export default function SinglePizzaPage({ data }) {
-  // ! CONTINUE ON PART 24
-  //   TODO: style this... list toppings
-  const { pizza } = data;
+const PizzaGrid = styled.div`
+  display: grid;
+  grid-gap: 2rem;
+  grid-template-columns: repeat(auto-fill, minmax(400px, 1fr));
+`;
+
+export default function SinglePizzaPage({ data: { pizza } }) {
   return (
-    <>
-      <p>{pizza.name}</p>
+    <PizzaGrid>
       <Img fluid={pizza.image.asset.fluid} alt={pizza.name} />
-    </>
+      <div>
+        <h2 className="mark">{pizza.name}</h2>
+        <ul>
+          {pizza.toppings.map((topping) => (
+            <li key={topping.id}>{topping.name}</li>
+          ))}
+        </ul>
+      </div>
+    </PizzaGrid>
   );
 }
 
